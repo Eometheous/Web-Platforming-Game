b92dd0dd8b3f9b46487eae84bc6a036b775ecc30
Minh Anh Ton <anhton20@gmail.com>
2024-02-09 12:53:48 -0800

    movement debug n zoom out cam
    + for now (cause im lazy) no horizontal input when not touching platform

--- dev/Unity/Assets/playerController.cs
+++ dev/Unity/Assets/playerController.cs
@@ -4,86 +4,69 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public float verticalForceMultiplier;
-    public float horizontalForceMultiplier;
-    public float maxVerticalVelocity;
-    public float maxHorizontalVelocity;
+    public float verticalForceMultiplier = 10f;
+    public float horizontalForceMultiplier = 5f;
+    public float maxVerticalVelocity = 10f;
+    public float maxHorizontalVelocity = 5f;
 
-    public float verticalOffset;
-    public bool touchingGround;
+    //public float horizontalInertia = 0.9f;
+    //private bool jumpLeft = false;
 
-    public KeyCode upKey;
-    public KeyCode downKey;
-    public KeyCode leftKey;
-    public KeyCode rightKey;
-
+    private bool touchingGround = false;
+    private KeyCode upKey = KeyCode.W;
+    private KeyCode downKey = KeyCode.S;
+    private KeyCode leftKey = KeyCode.A;
+    private KeyCode rightKey = KeyCode.D;
     private Rigidbody2D rb;
 
-    void Start()
-    {
-        verticalForceMultiplier = 10f;
-        horizontalForceMultiplier = 5f;
-        maxVerticalVelocity = 10f;
-        maxHorizontalVelocity = 5f;
-
-        verticalOffset = 5f;
-        touchingGround = false;
-
-        upKey = KeyCode.W;
-        downKey = KeyCode.S;
-        leftKey = KeyCode.A;
-        rightKey = KeyCode.D;
-
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float horizontalInput = 0f;
-        if (Input.GetKey(leftKey))
-        {
+        if (Input.GetKey(leftKey)) {
             horizontalInput = -1f;
-        } else if (Input.GetKey(rightKey))
-        {
+        } else if (Input.GetKey(rightKey)) {
             horizontalInput = 1f;
         }
+
         float verticalInput = 0f;
-        if (Input.GetKey(upKey) && touchingGround)
-        {
+        if (Input.GetKey(upKey) && touchingGround) {
             verticalInput = 1f;
         }
 
         Vector2 horizontalMovement = new Vector2(horizontalInput, 0f).normalized;
-        rb.AddForce(horizontalMovement * horizontalForceMultiplier);
+        if (touchingGround)
+            rb.AddForce(horizontalMovement * horizontalForceMultiplier);
         rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxHorizontalVelocity, maxHorizontalVelocity), rb.velocity.y);
 
-        Vector2 verticalMovement = new Vector2(0f, verticalInput * verticalOffset).normalized;
+        Vector2 verticalMovement = new Vector2(0f, verticalInput).normalized;
         rb.AddForce(verticalMovement * verticalForceMultiplier);
         rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -maxVerticalVelocity, maxVerticalVelocity));
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("Collided with object of tag: " + collision.gameObject.tag);
-        if (collision.gameObject.tag == "Platform")
-        {
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Player")
             touchingGround = true;
-        }
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
-        {
-            touchingGround = true;
-        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
-        {
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Player")
+            touchingGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Player")
             touchingGround = false;
-        }
+    }
+
+    public void changeKeys(KeyCode up, KeyCode down, KeyCode left, KeyCode right) {
+        upKey = up;
+        downKey = down;
+        leftKey = left;
+        rightKey = right;
     }
 }
