6c986063fc2892f0db9986f5890b90d16c3c416c
Logan Joven <33532422+logan-joven@users.noreply.github.com>
2024-02-29 09:43:09 -0800

    updated NavBar

--- dev/JALWebsite/client/src/Components/NavBar.svelte
+++ dev/JALWebsite/client/src/Components/NavBar.svelte
@@ -1,3 +1,32 @@
 <html lang="en">
-    <h1>NavBar goes here</h1>
+	<nav>
+		<h1>NavBar</h1>
+		<ul>
+			<li>
+					<a href="/">Home</a>
+			</li>
+		</ul>
+	</nav>
 </html>
+
+<style>
+	nav {
+		display:flex;
+		/* background-color: grey; */
+		
+	}
+	ul {
+		display: flex;
+		margin: 0;
+		margin-left: auto;
+		list-style:none;
+	}
+	li {
+		margin-right: 15px;
+	}
+	h1 {
+		margin: 0;
+		font-size: 1em;
+		font-weight: normal;
+	}
+</style>
