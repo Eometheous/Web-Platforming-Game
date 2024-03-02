536ddfd0a760408def7bdc9e5468b48c0e640e92
Minh Anh Ton <anhton20@gmail.com>
2024-03-02 15:54:39 -0800

    connect button to start game

--- dev/Unity/Assets/customizeManager.cs
+++ dev/Unity/Assets/customizeManager.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class customizeManager : MonoBehaviour
 {
-    
+    public string targetSceneName;
 
     void Start()
     {
-        
+        targetSceneName = "Basic Level";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void startGame()
     {
-        
+        SceneManager.LoadScene(targetSceneName);
     }
 }
