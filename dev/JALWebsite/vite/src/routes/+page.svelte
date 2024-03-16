91096f039a61b3832d465c37516729efe3e96fec
Minh Anh Ton <anhton20@gmail.com>
2024-03-16 13:24:45 -0700

    got unity to work in svelte

--- dev/JALWebsite/vite/src/routes/+page.svelte
+++ dev/JALWebsite/vite/src/routes/+page.svelte
@@ -1,35 +1,27 @@
-<script>
-	import { onMount } from 'svelte';
-	let htmlContent = '';
-
-	async function getGame() {
-        try {
-            const response = await fetch('http://127.0.0.1:8080/loadGame');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            htmlContent = await response.text();
-            console.log(htmlContent);
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-    }
-
-    onMount(() => {
-        getGame();
-    });
-</script>
-
 <svelte:head>
 	<title>Game</title>
 </svelte:head>
 
-<div class="renderedHTML">
-    {@html htmlContent}
+<div class="iframe-container">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <iframe frameborder="0" src="https://itch.io/embed-upload/9952218?color=333333" allowfullscreen="" width="1120" height="690">
+        <a href="https://byuntaeyeon02.itch.io/jal">Play Jump And Launch on itch.io</a>
+    </iframe>
 </div>
 
 <style>
 	@import 'tailwindcss/base';
 	@import 'tailwindcss/components';
 	@import 'tailwindcss/utilities';
+
+    .iframe-container {
+        padding-top: 20vh;
+        justify-content: center;
+        align-items: center;
+    }
+
+    iframe {
+        width: 1120px; 
+        height: 695px;
+    }
 </style>
